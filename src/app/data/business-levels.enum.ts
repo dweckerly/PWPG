5dc77df799a80f5db50897e2be5b9b816cd75834
David <d.weckerly@gmail.com>
2021-03-31 08:12:38 -0500

    data update

--- src/app/data/business-levels.enum.ts
+++ src/app/data/business-levels.enum.ts
@@ -1,7 +1,9 @@
-export enum BusinessLevels {
+export enum BusinessLevel {
     "Micro-enterprise",
     "Small Business",
     "Medium Business",
     "Large Business",
     "Corporation"
 }
+
+export type BusinessLevels = keyof typeof BusinessLevel;
